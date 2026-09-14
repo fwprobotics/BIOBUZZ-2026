2b3d0045f422dd44d8713125008e0b2d931581a4
Allison <67673392+Allybe@users.noreply.github.com>
2026-09-14 17:51:12 -0500

    added RR

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoperated/MainOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.teleoperated;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class MainOpMode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
